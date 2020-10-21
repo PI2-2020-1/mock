a6fbf731b85ac12663316a363c05f37b58c58692
Thiago Pereira <thiagorpereira7@gmail.com>
2020-10-21 01:46:01 -0300

    Add Cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import * as jsonServer from 'json-server';
 import * as routes from './routes.json';
 // import middlewares from './middlewares';
-
+import cors from 'cors';
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const port = 3002;
 
+server.use(cors())
 server.use(jsonServer.bodyParser);
 server.use(jsonServer.rewriter(routes));
 // server.use(middlewares);
